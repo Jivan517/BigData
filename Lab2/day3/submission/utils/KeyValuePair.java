2d682eaf122e8d5a9f8f756a5e9414196249e367
Jivan517 <np.jiv004@gmail.com>
2016-05-25 17:43:48 -0500

    submission files added for W1D3

---
+++ Lab2/day3/submission/utils/KeyValuePair.java
@@ -0,0 +1,39 @@
+package cs522.lab2.utils;
+
+public class KeyValuePair <K, V>{
+
+	private K key;
+	private V value;
+	
+	public KeyValuePair(K key, V value){
+		this.key = key;
+		this.value = value;
+	}
+	
+	public KeyValuePair(){
+		
+	}
+
+	public K getKey() {
+		return key;
+	}
+
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+	public V getValue() {
+		return value;
+	}
+
+	public void setValue(V value) {
+		this.value = value;
+	}
+	
+	@Override
+	public String toString(){
+		return "<" + key + "," + value + ">\n";
+	}
+	
+	
+}
