6652cc97ad8660ac8843980508b0e88f7bc08fbc
Jivan517 <np.jiv004@gmail.com>
2016-05-27 17:54:56 -0500

    stripe approach for word co-occurance solution added

--- Lab3/W1D5/code/src/cs522/lab3d5/Simulator.java
+++ Lab3/W1D5/code/src/cs522/lab3d5/Simulator.java
@@ -1,12 +1,12 @@
-package prob4;
+package cs522.lab3d5;
 
 
-public class JobExecutor {
+public class Simulator {
 
 	public static void main(String[] args) {
 
 		//Word Co-occurance Problem solution with Pair Approach  with m-mappers and r-reducers
-		StripeApproach wc = new StripeApproach(2, 4,
+		PairApproach wc = new PairApproach(2, 4,
 				new String[] { "./input/input1.txt", "./input/input2.txt"});
 		System.out.println("Number of Input-Splits: " + wc.getNumberOfInputSplits());
 		System.out.println("Number of Reducers: " + wc.getNumberOfReducers());
